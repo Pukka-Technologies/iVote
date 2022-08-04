c21d3016785f75cbc3f1447d04ed9c44234bfba9
mhira-codes <alhassansamiratu991@gmail.com>
2022-08-04 13:14:51 +0000

    minor changes

--- components/Login.js
+++ components/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
 
 
   return (
-    <div class="bg-lime-600   max-w-screen-2xl px-6  m-auto">
+    <div class="bg-green-400   max-w-screen-2xl px-6  m-auto">
 <div class='flex items-center justify-around flex-wrap'>
 <div  class='basis-[50%] min-w-[300px]'>
   <img src='/assets/Login-bro.png' alt='' class='max-w-full pt-12 pr-12' />
@@ -18,17 +18,17 @@ const Login = () => {
   <h1 class='text-5xl font-bold font-sans text-slate-900 pb-4'>Login</h1>
 <label for="email-address" class='text-2xl font-medium font-sans text-slate-900 '>Email</label>
 <div className='pt-2 pb-2'>
-  <input id="email" name="email" type="text" class='w-full md:w-[26rem] h-10  outline-none pl-3'/>
+  <input id="email" name="email" type="text" class='w-full md:w-[26rem] h-10  outline-none pl-3 rounded'/>
 </div>
 <label for="password" class='text-2xl font-medium font-sans text-slate-900'>Password</label>
 <div  className='pt-2'>
-  <input id="password" name="password" type="password" class='w-full md:w-[26rem] h-10  outline-none pl-3'/>
+  <input id="password" name="password" type="password" class='w-full md:w-[26rem] h-10  outline-none pl-3 rounded'/>
 </div>
 <div>
   <h3 class='pt-5 text-[darkslategray] text-2xl'>Forgot Password?</h3>
 </div>
 <div class='mt-6'> 
-  <button class='bg-[#001212] w-full md:w-64 h-10 text-neutral-200'>LOGIN</button></div>
+  <button class='bg-[#001212] w-full md:w-64 h-10 text-neutral-200 rounded'>LOGIN</button></div>
 
 
 
