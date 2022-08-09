122a6b809fe5cbd1ade18aa5d27cc83ac3dfd26a
Baaba <baabadampare@gmail.com>
2022-08-09 16:35:57 +0000

    add scroll to top

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,19 @@ import About from "../components/About";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ScrollArrow from "../components/ScrollArrow";
 import Steps from "../components/Steps";
 
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Features/>
-      <About/>
-      <Steps/>
-      <Footer/>
+      <Header />
+      {/* <ScrollArrow /> */}
+      <Features />
+      <About />
+      <Steps />
+      <Footer />
     </div>
   );
 }
