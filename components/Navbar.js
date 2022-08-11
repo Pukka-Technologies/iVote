7a73f8b2bca851318984c21ee45142bb060c394d
Baaba <baabadampare@gmail.com>
2022-08-11 09:25:23 +0000

    add events on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import {AiOutlineUser} from "react-icons/ai"
+import React from "react";
+import Link from "next/link";
+import { AiOutlineUser } from "react-icons/ai";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className='flex flex-row justify-around text-white pt-9 font-text'>
+      <div className="flex flex-row justify-around text-white pt-9 font-text">
         <div className="font-extrabold uppercase text-2xl">Logo</div>
         <div>
-          <ul className='flex flex-row gap-8 text-lg'>
+          <ul className="flex flex-row gap-8 text-lg">
             <li>Home</li>
             <li>Terms and Conditions</li>
-            <li>Contests</li>
+            <Link href={"/events"}>
+              <li>Events</li>
+            </Link>
             <li>Contact Us</li>
           </ul>
         </div>
-        <div className='flex flex-row gap-2 text-lg'>
-            <AiOutlineUser/>
-            <AiOutlineSearch/>
+        <div className="flex flex-row gap-2 text-lg">
+          <AiOutlineUser />
+          <AiOutlineSearch />
         </div>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
