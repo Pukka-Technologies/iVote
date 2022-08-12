f89b7fac8907db25bd3835343146c3830307cf5a
Baaba <baabadampare@gmail.com>
2022-08-12 13:16:28 +0000

    start editing contestants

--- pages/contestants.js
+++ pages/contestants.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Contestants from '../components/Contestants'
-import ContestsHeader from '../components/ContestsHeader'
+import React from "react";
+import Contestants from "../components/Contestants";
+import ContestsHeader from "../components/ContestsHeader";
+import Footer from "../components/Footer";
 
 const contestants = () => {
   return (
     <div>
-        <ContestsHeader/>
-        <Contestants/>
-        <Footer/>
+      <ContestsHeader />
+      <Contestants />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default contestants
+export default contestants;
