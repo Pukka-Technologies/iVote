86734064674901e1a3ebc6b6da28491610aa9f97
Baaba <baabadampare@gmail.com>
2022-08-17 12:40:27 +0000

    add states for mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { AiOutlineSearch, AiOutlineBars, AiOutlineUser } from "react-icons/ai";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  let menu 
+
+  if (showMenu){
+    menu = (
+      <div>
+        <ul className="flex flex-col gap-8 text-lg">
+          <Link href={"/"}>
+            <li>Home</li>
+          </Link>
+          <Link href={"/events"}>
+            <li>Events</li>
+          </Link>
+          <li>Terms and Conditions</li>
+          <li>Contact Us</li>
+        </ul>
+        <AiOutlineUser />
+      </div>
+    );
+  }
+
   return (
     <nav>
       <div className="flex flex-row md:justify-around justify-between px-4 md:px-0 text-white pt-9 font-text cursor-pointer">
@@ -22,9 +44,11 @@ const Navbar = () => {
         <div className="flex flex-row gap-2 text-lg">
           <AiOutlineUser />
           <AiOutlineSearch />
-          <span className="md:hidden"><AiOutlineBars/></span>
+          <span className="md:hidden"><AiOutlineBars onClick={() => setShowMenu(!showMenu)}/></span>
         </div>
       </div>
+
+      {menu}
     </nav>
   );
 };
