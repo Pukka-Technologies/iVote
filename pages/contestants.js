fcbd07c0dd3c78a429c59674bf83ea0fb3560d1e
PAUL ISREAL <piduyoh@st.ug.edu.gh>
2022-08-30 15:14:44 +0000

    change

--- pages/contestants.js
+++ pages/contestants.js
@@ -24,7 +24,7 @@ const Contestantpage = () => {
                 </h3> */}
                 <div className="relative">
                   {/* CONTESTANTS IMAGE GOES HERE */}
-                  <div className="w-full h-80  rounded-xl">
+                  <div className="w-full h-80 rounded-xl">
                     <img
                       className="w-full h-full object-contain"
                       src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
