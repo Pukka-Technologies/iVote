f96d50409b6779a36db7f4a9a18407eb564cdd82
Baaba <baabadampare@gmail.com>
2022-09-08 13:34:55 +0000

    add no data svg

--- components/Promises/index.js
+++ components/Promises/index.js
@@ -4,7 +4,7 @@ import React from "react";
 export const Empty = ({text}) => {
   return (
     <div className="flex flex-col min-h-[50vh] items-center justify-center bg-transparent font-text mt-6 px-[2em] py-[1em] rounded-lg">
-        <Image src="/assets/empty.svg" width={200} height={200} alt="empty" />
+        <Image src="/assets/no-data.svg" width={200} height={200} alt="empty" />
         <p className="text-base font-bold mt-5">{text}</p>
     </div>
   );
@@ -12,7 +12,7 @@ export const Empty = ({text}) => {
 export const Fetching = ({text}) => {
   return (
     <div className="flex flex-col min-h-[50vh] items-center justify-center bg-transparent font-text mt-6 px-[2em] py-[1em] rounded-lg">
-        <Image src="/assets/NotFound.svg" width={200} height={200} alt="empty" />
+        <Image src="/assets/no-data.svg" width={200} height={200} alt="empty" />
         <p className="text-base font-bold mt-5">{text}</p>
     </div>
   );
