9ce4dae5264336554543eeaad2575ded50b12dbb
qbentil <bentilshadrack72@gmail.com>
2022-09-13 20:00:22 +0000

    Remove redundant data functions

--- utils/functions.js
+++ utils/functions.js
@@ -15,6 +15,20 @@ export const fetchData = async (route, callback) => {
     }
 
 }
+export const fetchLeaderboard = async (body, callback) => {
+  try {
+      const { data } = await Axios({
+          method: "POST",
+          url: "leaderboard",
+          data: body
+      })
+      callback(data)
+
+  } catch (error) {
+      console.log(error)
+  }
+
+}
 
 export const castVote = async (data, callback) => {
     try {
@@ -32,37 +46,3 @@ export const castVote = async (data, callback) => {
 
 }
 
-export const generateLeaderboard = (contestants, votes) => {
-    const updatedContestants = contestants.map((contestant) => {
-      const {online_votes, offline_votes} = getContestantTotalVotes(votes, contestant._id);
-      // remove votes from contestant
-      let total_votes = online_votes + offline_votes;
-      delete contestant.votes;
-      return { ...contestant, total_votes, online_votes, offline_votes };
-    });
-
-    console.log(">>>>",contestants)
-
-    return {
-      leaderboard: updatedContestants,
-      total_votes_cast: updatedContestants.reduce(
-        (total, contestant) => total + contestant.total_votes,
-        0
-      ),
-    };
-  };
-  
-  // calculate total votes for each contestant from votes array
-  export const getContestantTotalVotes = (votes, id) => {
-    const items = votes.filter((vote) => vote.contestant_id == id);
-    let online_votes = 0;
-    let offline_votes = 0;
-    items.forEach((item) => {
-      if(item.type == "online"){
-        online_votes += item.total_votes;
-      }else{
-        offline_votes += item.total_votes;
-      }
-    })
-    return {online_votes, offline_votes};
-  }
