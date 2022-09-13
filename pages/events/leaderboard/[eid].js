ad4f9e9ef349cebd6838ad2a1a91af5fd7d99757
ksowah <sowahkelvin640@gmail.com>
2022-09-13 15:54:48 +0000

    event leaderboard

--- pages/events/leaderboard/[eid].js
+++ pages/events/leaderboard/[eid].js
@@ -1,31 +1,58 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import EventsHeader from '../../../components/EventsHeader';
+import Footer from '../../../components/Footer';
+import LeaderboardTable from '../../../components/LeaderboardTable';
 import { useStateValue } from '../../../context/StateProvider'
+import {fetchData, generateLeaderboard} from "../../../utils/functions"
 
 const EventBoard = () => {
 
-    const [{contestants, votes}, dispatch] = useStateValue()
+    const [{contestants}, dispatch] = useStateValue()
+
+    const [leaderboard, setLeaderboard] = useState(null)
+    const [totalVotes, setTotalVotes] = useState(null)
+
+    const router = useRouter()
+    const event_id = router.query
 
     const fetchLeaderboard = (params) => {
-        const { event_id } = params;
+
+      fetchData("vote", async (data) => {
+        const { _id } = event_id;
         const year = new Date().getFullYear();
-        const event_votes = votes.filter(
-          (vote) => vote.event_id == event_id && vote.year == year
+        const event_votes = data.data.filter(
+          (vote) => vote.event_id === _id && vote.year == year
         );
         const event_contestants = contestants.filter(
-          (contestant) => contestant.event_id == event_id && contestant.year == year
+          (contestant) => contestant.event_id == _id && contestant.year == year
         );
         // generate leaderboard for contestants in event
         const { leaderboard, total_votes_cast } = generateLeaderboard(
           event_contestants,
           event_votes
         );
+
+        console.log("leaderboard", leaderboard);
+
         setLeaderboard(leaderboard);
         setTotalVotes(total_votes_cast);
+      })
+
       };
     
 
+      useEffect(() => {
+        fetchLeaderboard()
+      }, [])
+      
+
   return (
-    <div>EventBoard</div>
+    <>
+      <EventsHeader />
+        <LeaderboardTable leaderboard={leaderboard} />
+      <Footer/>
+    </>
   )
 }
 
