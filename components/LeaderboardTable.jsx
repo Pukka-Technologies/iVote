7a73cb925405c354d51ee5d47af353ccc6867851
ksowah <sowahkelvin640@gmail.com>
2022-09-13 16:14:24 +0000

    restyle table

--- components/LeaderboardTable.jsx
+++ components/LeaderboardTable.jsx
@@ -2,42 +2,42 @@ import Image from "next/image";
 
 const LeaderboardTable = ({leaderboard}) => {
   return (
-    <div className="w-full flex flex-col items-center justify-center p-8 my-16 overflow-x-auto">
+    <div className="w-full flex flex-col md:px-14 items-center justify-center p-8 my-16 overflow-x-auto">
       
-<div className="relative shadow-md sm:rounded-lg">
+<div className="relative w-full shadow-md sm:rounded-lg">
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:text-gray-500">
             <tr>
                 <th scope="col" className="py-3 px-6"></th>
                 <th scope="col" className="py-3 px-6">
                     Contestant
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className="py-3 px-6 text-center">
                     Category
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className="py-3 px-6 text-center">
                     Votes
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className="py-3 px-6 text-center">
                     <span className="sr-only">Edit</span>
                 </th>
             </tr>
         </thead>
         <tbody>
-            {leaderboard?.map(contestant => (
-                <tr key={contestant._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            {leaderboard?.map((contestant, idx) => (
+                <tr key={contestant._id} className={`border-b ${idx % 2 > 0 ? "bg-gray-100" : "text-gray-50 bg-green-300"} dark:border-gray-50`}>
                 <td className="py-2 px-6">
-                    <div className="relative w-12 h-12 rounded-md">
+                    <div className="relative w-12 h-12 rounded-md overflow-hidden">
                         <Image src={contestant.imageURL} layout="fill" objectFit="cover" />
                     </div>
                 </td>
-                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap">
                     {contestant.name}
                 </th>
-                <td className="py-4 px-6">
+                <td className="py-4 px-6 text-center">
                     Laptop
                 </td>
-                <td className="py-4 px-6">
+                <td className="py-4 px-6 text-center ">
                     {contestant.total_votes}
                 </td>
                 <td className="py-4 px-6 text-right">
