5bf49846576fa222ad283fd5609e77f695e975e4
Bentil Shadrack <bentilshadrack72@gmail.com>
2023-05-10 15:51:52 +0000

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://pvotes-backend.herokuapp.com/" || "http://localhost:8800/",
+    baseURL: "https://ivote-api.onrender.com",
     withCredentials: true
 })
