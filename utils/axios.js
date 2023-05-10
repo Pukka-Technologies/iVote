911eecfabe159bad948e39d2f6390e017be90186
Bentil Shadrack <bentilshadrack72@gmail.com>
2023-05-10 16:13:16 +0000

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://ivote-api.onrender.com",
+    baseURL: "https://ivote-api.onrender.com/",
     withCredentials: true
 })
