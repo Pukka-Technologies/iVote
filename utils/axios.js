53a46ccafbddcc2e0e3aeaa1d2e4a27f6dba462b
Bentil Shadrack <bentilshadrack72@gmail.com>
2024-06-20 08:45:57 +0000

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://ivote-backend-0bxf.onrender.com/",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8800/",
     withCredentials: true
 })
